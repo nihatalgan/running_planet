2d331e87a072334b6d274f46c2028ba7d0251979
Ensha C Latheef <enshalatheef@gmail.com>
2024-01-10 22:19:29 +0100

    added basic routes

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.use("/", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const userRoutes = require("./routes/user.routes");
+app.use("/user", userRoutes)
+
+const eventRoutes = require("./routes/event.routes");
+app.use("/event", eventRoutes)
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ routes/user.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET user profile page */
+router.get("/user/profile", (req, res, next) => {
+  res.render("index");
+});
+
+/* GET user profile edit page */
+router.get("/user/profile/edit", (req, res, next) => {
+    res.render("index");
+});
+
+/* POST user profile edit - handling the data from user profile edit form*/
+router.get("/user/profile/edit", (req, res, next) => {
+    res.render("index");
+});
+
+
+module.exports = router;
