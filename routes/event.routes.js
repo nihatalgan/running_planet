61a9d2940cc66a2c8807bfebb35f130288978165
Ensha C Latheef <enshalatheef@gmail.com>
2024-01-12 22:37:17 +0100

    updated routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -2,39 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 /* GET - show events listing page */
-router.get("/event/list", (req, res, next) => {
+router.get("/list", (req, res, next) => {
   res.render("index");
 });
 
 /* GET - show event details page */
-router.get("/event/:id", (req, res, next) => {
+router.get("/:id", (req, res, next) => {
     res.render("index");
 });
 
 /* GET - show event create page */
-router.get("/event/create", (req, res, next) => {
+router.get("/create", (req, res, next) => {
     res.render("index");
 });
 
 /* POST - event create - handling the data from event create form*/
-router.post("/event/create", (req, res, next) => {
+router.post("/create", (req, res, next) => {
     res.render("index");
 });
 
 
 /* GET - show user profile edit page */
-router.get("/event/:id/edit", (req, res, next) => {
+router.get("/:id/edit", (req, res, next) => {
     res.render("index");
 });
 
 /* POST - event edit - handling the data from event edit form*/
-router.post("/event/:id/edit", (req, res, next) => {
+router.post("/:id/edit", (req, res, next) => {
     res.render("index");
 });
 
 
 /* POST - event delete - handling the data for event deletion*/
-router.post("/event/:id/delete", (req, res, next) => {
+router.post("/:id/delete", (req, res, next) => {
     res.render("index");
 });
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 /* GET user profile page */
-router.get("/user/profile", (req, res, next) => {
+router.get("/profile", (req, res, next) => {
   res.render("index");
 });
 
 /* GET user profile edit page */
-router.get("/user/profile/edit", (req, res, next) => {
+router.get("/profile/edit", (req, res, next) => {
     res.render("index");
 });
 
 /* POST user profile edit - handling the data from user profile edit form*/
-router.post("/user/profile/edit", (req, res, next) => {
+router.post("/profile/edit", (req, res, next) => {
     res.render("index");
 });
 
