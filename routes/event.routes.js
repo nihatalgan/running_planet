68fdd0ad8abbbe4e4450cfca815f4989bb1de741
Nihat Algan <burakalgan@gmail.com>
2024-01-20 09:43:03 +0100

    add feature to create review

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const isEventOrganiser = require("../middleware/isEventOrganiser");
 
-
 // Require the Event model in order to interact with the database
 const Event = require("../models/Event.model");
 // ********* require fileUploader in order to use it *********
 const fileUploader = require("../config/cloudinary.config");
+const Review = require("../models/Review.model");
 
 /* GET - show event create page */
 router.get("/create", isLoggedIn, (req, res, next) => {
@@ -85,7 +85,7 @@ router.get("/:id/edit", isLoggedIn, isEventOrganiser, (req, res, next) => {
 
   Event.findById(id)
     .then((eventToEdit) => {
-      res.render("event/eventedit", eventToEdit)
+      res.render("event/eventedit", eventToEdit);
     })
     .catch((error) =>
       console.log(`Error while getting a single event for edit: ${error}`)
@@ -95,7 +95,7 @@ router.get("/:id/edit", isLoggedIn, isEventOrganiser, (req, res, next) => {
 /* POST - event edit - handling the data from event edit form*/
 router.post(
   "/:id/edit",
-  isLoggedIn, 
+  isLoggedIn,
   isEventOrganiser,
   fileUploader.single("event-cover-image"),
   (req, res, next) => {
@@ -135,6 +135,37 @@ router.post("/:id/delete", isLoggedIn, isEventOrganiser, (req, res, next) => {
   Event.findByIdAndDelete(id)
     .then(() => res.redirect("/event/list"))
     .catch((err) => console.log(err));
+});
+
+/* POST - add comment - handling the data from event edit form*/
+router.post("/:id/comment", isLoggedIn, (req, res, next) => {
+  let author = req.session.currentUser._id;
+  let event = req.params.id;
+  let rating = req.body.rating;
+  let comment = req.body.comment;
+  Review.create({
+    author,
+    event,
+    rating,
+    comment,
+  })
+    .then((newComment) => {
+      console.log(newComment);
+      res.redirect(`/event/${event}`);
+    })
+    .catch((error) =>
+      console.log(`Error while creating a new event: ${error}`)
+    );
+
+  // Event.findByIdAndUpdate(
+  //   event,
+  //   { name, date, location, distance, description, website, imageUrl },
+  //   { new: true }
+  // )
+  //   .then((eventEdited) => res.redirect(`/event/${eventEdited.id}`))
+  //   .catch((error) =>
+  //     console.log(`Error while getting a single event for edit: ${error}`)
+  //   );
 });
 
 module.exports = router;
